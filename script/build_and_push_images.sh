9a4b4e4b7beca86b9265e01046c7bd705c8d67bf
cbluebird <crk2778355976@163.com>
2024-07-31 22:33:51 +0800

    test

--- script/build_and_push_images.sh
+++ script/build_and_push_images.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-# # 从环境变量中读取DIFF_OUTPUT和PARENT_DIRS
-# DIFF_OUTPUT=${{ env.DIFF_OUTPUT }}
-# PARENT_DIRS=${{ env.PARENT_DIRS }}
-
+# 打印环境变量以进行调试
 echo "PARENT_DIRS=$PARENT_DIRS"
 echo "DIFF_OUTPUT=$DIFF_OUTPUT"
 
-IFS=',' read -r -a DIFF_OUTPUT <<< $PARENT_DIRS
-IFS=',' read -r -a PARENT_DIRS <<< $DIFF_OUTPUT
+# 将环境变量读取为数组
+IFS=',' read -r -a DIFF_OUTPUT_ARRAY <<< "$DIFF_OUTPUT"
+IFS=',' read -r -a PARENT_DIRS_ARRAY <<< "$PARENT_DIRS"
 
-echo $DIFF_OUTPUT
-echo $PARENT_DIRS
+# 打印数组内容以进行调试
+echo "DIFF_OUTPUT array: ${DIFF_OUTPUT_ARRAY[@]}"
+echo "PARENT_DIRS array: ${PARENT_DIRS_ARRAY[@]}"
 
 # 构建并推送每个Docker镜像
-for i in "${!DIFF_OUTPUT[@]}"; do
-  DOCKERFILE_PATH=${DIFF_OUTPUT[$i]}
-  PARENT_DIR=${PARENT_DIRS[$i]}
+for i in "${!DIFF_OUTPUT_ARRAY[@]}"; do
+  DOCKERFILE_PATH=${DIFF_OUTPUT_ARRAY[$i]}
+  PARENT_DIR=${PARENT_DIRS_ARRAY[$i]}
   TAG="devbox-$PARENT_DIR:latest"
-  echo "Building and pushing image for $DOCKERFILE_PATH with tag $TAG And User as $DOCKER_USERNAME"
+  echo "Building and pushing image for $DOCKERFILE_PATH with tag $TAG and user as $DOCKER_USERNAME"
   docker buildx build --push \
     --file $DOCKERFILE_PATH \
     --tag "$DOCKER_USERNAME/$TAG" \
--- script/get_changed_files.sh
+++ script/get_changed_files.sh
@@ -5,16 +5,16 @@ DIFF_OUTPUT=$(git diff --name-only "$1" "$2"|grep Dockerfile)
 
 # 提取上一级目录
 PARENT_DIRS=()
-FILE_PATH=()
+FILE_PATHS=()
 for FILE_PATH in $DIFF_OUTPUT; do
   PARENT_DIR=$(dirname "$FILE_PATH" | awk -F'/' '{print $(NF)}')
   PARENT_DIRS+=("$PARENT_DIR")
-  FILE_PATH+=("$FILE_PATH")
+  FILE_PATHS+=("$FILE_PATH")
 done
 
 # 将数组转换为字符串
 PARENT_DIRS_STRING=$(IFS=,; echo "${PARENT_DIRS[*]}")
-DIFF_OUTPUT_STRING=$(IFS=,; echo "${FILE_PATH[*]}")
+DIFF_OUTPUT_STRING=$(IFS=,; echo "${FILE_PATHS[*]}")
 echo "PARENT_DIRS=$PARENT_DIRS_STRING"
 echo "DIFF_OUTPUT=$DIFF_OUTPUT_STRING"
 
