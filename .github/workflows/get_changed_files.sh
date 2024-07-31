4ac80934b063a46da4050e0b954e5d2fa9645555
cbluebird <crk2778355976@163.com>
2024-07-31 17:41:35 +0800

    shell test

---
+++ .github/workflows/get_changed_files.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 获取变更的文件列表
+DIFF_OUTPUT=$(git diff --name-only "$1" "$2" | grep 'Dockerfile')
+
+# 提取上一级目录
+PARENT_DIRS=()
+for FILE_PATH in $DIFF_OUTPUT; do
+  PARENT_DIR=$(dirname "$FILE_PATH" | awk -F'/' '{print $(NF-1)}')
+  PARENT_DIRS+=("$PARENT_DIR")
+done
+
+# 将数组转换为字符串
+PARENT_DIRS_STRING=$(IFS=,; echo "${PARENT_DIRS[*]}")
+DIFF_OUTPUT_STRING=$(IFS=,; echo "${DIFF_OUTPUT[*]}")
+
+echo "PARENT_DIRS=$PARENT_DIRS_STRING"
+echo "DIFF_OUTPUT=$DIFF_OUTPUT_STRING"
+
+# 输出到环境变量
+echo "PARENT_DIRS=$PARENT_DIRS_STRING" >> $GITHUB_ENV
+echo "DIFF_OUTPUT=$DIFF_OUTPUT_STRING" >> $GITHUB_ENV
