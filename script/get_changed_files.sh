e9749fc53caa6277c66845ab9f1f433f02c079aa
cbluebird <crk2778355976@163.com>
2024-07-31 22:19:56 +0800

    test

--- script/get_changed_files.sh
+++ script/get_changed_files.sh
@@ -5,15 +5,16 @@ DIFF_OUTPUT=$(git diff --name-only "$1" "$2"|grep Dockerfile)
 
 # 提取上一级目录
 PARENT_DIRS=()
+FILE_PATH=()
 for FILE_PATH in $DIFF_OUTPUT; do
   PARENT_DIR=$(dirname "$FILE_PATH" | awk -F'/' '{print $(NF)}')
   PARENT_DIRS+=("$PARENT_DIR")
+  FILE_PATH+=("$FILE_PATH")
 done
 
 # 将数组转换为字符串
 PARENT_DIRS_STRING=$(IFS=,; echo "${PARENT_DIRS[*]}")
-#DIFF_OUTPUT_STRING=$(IFS=,; echo "${DIFF_OUTPUT[*]}")
-DIFF_OUTPUT_STRING=$("$DIFF_OUTPUT"|tr '/n' ',')
+DIFF_OUTPUT_STRING=$(IFS=,; echo "${FILE_PATH[*]}")
 echo "PARENT_DIRS=$PARENT_DIRS_STRING"
 echo "DIFF_OUTPUT=$DIFF_OUTPUT_STRING"
 
