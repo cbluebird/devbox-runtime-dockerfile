d8fe9429097ce02891c5586edda1d251f86380ff
cbluebird <crk2778355976@163.com>
2024-08-05 14:34:29 +0800

    chore: Update Dockerfile to include Apache Maven 3.8.6 and improve build process for Docker image push to ghcr

--- script/build_and_push_images.sh
+++ script/build_and_push_images.sh
@@ -21,6 +21,6 @@ for i in "${!DIFF_OUTPUT_ARRAY[@]}"; do
   docker buildx build --push \
     --file $DOCKERFILE_PATH \
     --platform linux/amd64\
-    --tag "ghcr.io/$DOCKER_USERNAME/$TAG" \
+    --tag "ghcr.io/$DOCKER_USERNAME/devbox-runtime-dockerfile/$TAG" \
     .
 done
