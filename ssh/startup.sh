a35b49a075a37490a6179b4648a8cc320178c36e
cbluebird <crk2778355976@163.com>
2024-08-20 16:16:25 +0800

    chore: Update SSH startup script to read password from file or generate a random one

--- ssh/startup.sh
+++ ssh/startup.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+# Define the password file location
+PASSWORD_FILE="/usr/start/user_password.txt"
+
+# Check if the password file exists
+if [ -f "${PASSWORD_FILE}" ]; then
+    # If the password file exists, read the password from it
+    USER_PASSWORD=$(cat ${PASSWORD_FILE})
+else
+    # If the password file doesn't exist, check if USER_PASSWORD is already set
+    if [ -z "${USER_PASSWORD}" ]; then
+        # If USER_PASSWORD is not set, generate a random 8-character password
+        USER_PASSWORD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c8)
+    fi
+    # Save the generated or existing USER_PASSWORD to the password file
+    mkdir -p "$(dirname "${PASSWORD_FILE}")"
+    echo "${USER_PASSWORD}" > "${PASSWORD_FILE}"
+    # Set the password for the 'sealos' user
+    echo "sealos:${USER_PASSWORD}" | sudo chpasswd
+fi
+
+# Display the password for logging purposes (optional)
 echo "USER_PASSWORD=${USER_PASSWORD}"
 
-# 创建用户并设置密码
-sudo echo "sealos:${USER_PASSWORD}" | chpasswd
-
+# Start the SSH daemon
 /usr/sbin/sshd 
-
-sleep Inf
