0d09514e044f40f8af693e7f9d1f777dba43907f
cbluebird <crk2778355976@163.com>
2024-08-20 17:25:49 +0800

    chore: Update SSH startup script to use SEALOS_DEVBOX_PASSWORD variable for password

--- ssh/startup.sh
+++ ssh/startup.sh
@@ -4,24 +4,19 @@
 PASSWORD_FILE="/usr/start/user_password.txt"
 
 # Check if the password file exists
-if [ -f "${PASSWORD_FILE}" ]; then
-    # If the password file exists, read the password from it
-    USER_PASSWORD=$(cat ${PASSWORD_FILE})
-else
+if [ ! -f "${PASSWORD_FILE}" ]; then
     # If the password file doesn't exist, check if USER_PASSWORD is already set
-    if [ -z "${USER_PASSWORD}" ]; then
+    if [ -z "${SEALOS_DEVBOX_PASSWORD}" ]; then
         # If USER_PASSWORD is not set, generate a random 8-character password
-        USER_PASSWORD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c8)
+        SEALOS_DEVBOX_PASSWORD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c8)
     fi
     # Save the generated or existing USER_PASSWORD to the password file
-    mkdir -p "$(dirname "${PASSWORD_FILE}")"
-    echo "${USER_PASSWORD}" > "${PASSWORD_FILE}"
+    touch "${PASSWORD_FILE}"
     # Set the password for the 'sealos' user
-    echo "sealos:${USER_PASSWORD}" | sudo chpasswd
+    echo "sealos:${SEALOS_DEVBOX_PASSWORD}" | sudo chpasswd
+    # Display the password for logging purposes (optional)
+    echo "SEALOS_DEVBOX_PASSWORD=${SEALOS_DEVBOX_PASSWORD}"
 fi
-
-# Display the password for logging purposes (optional)
-echo "USER_PASSWORD=${USER_PASSWORD}"
 
 # Start the SSH daemon
 /usr/sbin/sshd 
